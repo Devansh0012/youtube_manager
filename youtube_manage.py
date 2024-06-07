73a980a6d8e1aa80b306bd49247eb5279cc17e67
Devansh0012 <devanshdubey0012@gmail.com>
2024-06-07 13:06:02 +0530

    update code file

--- youtube_manage.py
+++ youtube_manage.py
@@ -1,16 +1,61 @@
+import json
+
+def load_data():
+    try:
+        with open("youtube.txt", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = []
+    return data
+
+def save_data_helper(videos):
+    with open("youtube.txt", "w") as file:
+        json.dump(videos, file)
+
 def list_all_videos(videos):
-    pass
+    print("\n")
+    print("*"*70)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['title']}")
+        print(f"Time: {video['time']}")
+        print(f"URL: {video['url']}")
+    print("\n")
+    print("*"*70)
 
-def add_video(video):
-    pass
+def add_video(videos):
+    title = input("Enter the video title: ")
+    time = input("Enter the video time: ")
+    url = input("Enter the video URL: ")
+    video = ({
+        "title": title,
+        "time": time,
+        "url": url
+    })
+    
+    videos.append(video)
+    save_data_helper(videos)
+    print("Video added successfully! ")
 
-def update_video(video):
-    pass
+def update_video(videos):
+    list_all_videos(videos)
+    index = int(input("Enter the video index to update: "))
+    video = videos[index - 1]
+    
+    video["title"] = input("Enter the new title: ")
+    video["time"] = input("Enter the new time: ")
+    video["url"] = input("Enter the new URL: ")
+    save_data_helper(videos)
+    print("Video updated successfully! ")
 
-def delete_video(video):
-    pass
+def delete_video(videos):
+    list_all_videos(videos)
+    index = int(input("Enter the video index to delete: "))
+    videos.pop(index - 1)
+    save_data_helper(videos)
+    print("Video deleted successfully! ")
+
 def main():
-    videos = []
+    videos = load_data()
 
     while True:
         print("\n YouTube Manager | choose an option ")
@@ -18,7 +63,7 @@ def main():
         print("2. Add a YouTube video ")
         print("3. Update a YouTube video details ")
         print("4. Delete a YouTube video ")
-        print("5. Exite the app ")
+        print("5. Exit the app ")
         
         choice = input("Enter your choice: ")
         
